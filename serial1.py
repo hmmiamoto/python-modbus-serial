9c923d7fc450c49b7f2e108d54b47f51993febf4
Henrique Miamoto <mitomu@gmail.com>
2020-06-28 21:15:15 -0300

    Proxsys Rele Funcionando

--- serial1.py
+++ serial1.py
@@ -1,22 +1,37 @@
+from pymodbus.server.sync import StartSerialServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSparseDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.transaction import ModbusRtuFramer, ModbusBinaryFramer
+
 import serial
 import crc16
 import time
+import logging
 
-# print("Hello World")
+FORMAT = ('%(asctime)-15s %(threadName)-15s'
+          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+
 # print(crc16.crc16xmodem(b'123456789'))
 # print(crc16.crc16xmodem(b"\x01\x06\x00\x01\x01\x00"))
 # print(b"\x01\x06\x00\x01\x01\x00")
 
 
-with serial.Serial(port='/dev/ttyUSB0', 
+with serial.Serial(
+    port='/dev/ttyUSB0', 
     baudrate=9600, 
     bytesize=serial.EIGHTBITS, 
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE
     ) as ser:
     ser.flush()
+
     
-        
+    print("Ligando")
     # Turn on - SLAVE 1
     # \xUnit_ID\0xFC\0xAdress\0xAdress\0xValue\0xValue\0xCRC16\0xCRC16
     # Adress, Value and CRC16 = 2 Bytes
@@ -36,6 +51,7 @@ with serial.Serial(port='/dev/ttyUSB0',
     time.sleep(1)
     ser.write(b"\x01\x06\x00\x08\x01\x00\x09\x98")
     time.sleep(1)
+    
 
     # Turn on - SLAVE 2
     ser.write(b"\x02\x06\x00\x01\x01\x00\xD9\xA9")
@@ -71,16 +87,19 @@ with serial.Serial(port='/dev/ttyUSB0',
     ser.write(b"\x02\x06\x00\x10\x01\x00\x89\xAC")
     time.sleep(5)
 
-    # Turn on - SLAVE 5
-    ser.write(b"\x05\x05\x00\x00\x00\x01\x0D\x8E")
-    time.sleep(1)
-    ser.write(b"\x05\x05\x00\x01\x00\x01\x5C\x4E")
-    time.sleep(1)
-    ser.write(b"\x05\x05\x00\x02\x00\x01\xAC\x4E")
-    time.sleep(1)
-    ser.write(b"\x05\x05\x00\x03\x00\x01\xFD\x8E")
-    time.sleep(5)
 
+    # Turn on - SLAVE 5
+    ser.write(b"\x05\x05\x00\x00\xFF\x00\xBE\x8D")
+    time.sleep(1)
+    ser.write(b"\x05\x05\x00\x01\xFF\x00\xDC\x7E")
+    time.sleep(1)
+    ser.write(b"\x05\x05\x00\x02\xFF\x00\x2C\x7E")
+    time.sleep(1)
+    ser.write(b"\x05\x05\x00\x03\xFF\x00\x7D\xBE")
+    time.sleep(5)
+ 
+   
+    print("Desligando")
     # Turn off - SLAVE 1
     ser.write(b"\x01\x06\x00\x01\x02\x00\xD9\x6A")
     time.sleep(1)
@@ -135,6 +154,7 @@ with serial.Serial(port='/dev/ttyUSB0',
     ser.write(b"\x02\x06\x00\x10\x02\x00\x89\x5C")
     time.sleep(5)
 
+
     # Turn off - SLAVE 5
     ser.write(b"\x05\x05\x00\x00\x00\x00\xCC\x4E")
     time.sleep(1)
@@ -144,15 +164,20 @@ with serial.Serial(port='/dev/ttyUSB0',
     time.sleep(1)
     ser.write(b"\x05\x05\x00\x03\x00\x00\x3C\x4E")
     time.sleep(5)
-    
 
+ 
+    print("Analogico")
     # Read Proxsys Analog Input 1
-    ser.write(b"\x05\x03\x00\x0F\x00\x01\xB5\x8D")
-    AnalogIn1 = ser.Read(8)
+    ser.write(b"\x05\x03\x00\x0E\x00\x01\x4D\xE4")
+    # time.sleep(1)
+    AnalogIn1 = ser.read(9)
+    # AnalogIn1 = ser.read_until(expected=b'\n', size=None)
+    # AnalogIn1 = ser.read_until(b'\n')
+    print("EA 1 e 2")
     print(AnalogIn1)
 
 
     # Read Proxsys Analog Input 2
-    ser.write(b"\x05\x03\x00\x10\x00\x01\xB5\x8D")
-    AnalogIn2 = ser.Read(8)
+    ser.write(b"\x05\x03\x00\x0F\x00\x01\xB5\x8D")
+    AnalogIn2 = ser.read(20)
     print(AnalogIn2)
